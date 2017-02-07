67c24cb3cb2d99aab90ac0a51c912621391265ba
Ranjan Bajracharya <ranjanbajra@gmail.com>
2017-02-07 07:56:26 +0000

    CRUD action applied to postcontroller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+    def index
+    end
+    
+    def show
+    end
+    
+    def new
+    end
+    
+    def new
+    end
+    
+    def create
+    end
+    
+    def edit
+    end
+    
+    def updated
+    end
+    
+    def destroy
+    end
+
 end
