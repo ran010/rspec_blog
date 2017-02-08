b0d4dd03ddc2af9a8a510c17a998c771fe954e57
Ranjan Bajracharya <ranjanbajra@gmail.com>
2017-02-08 12:54:26 +0000

    shoulda-matcher gem added to check validation

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'shoulda-matchers', '~> 3.1'
   gem 'byebug'
 end
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,24 +4,46 @@ class PostsController < ApplicationController
     end
     
     def show
+        @posts = Post.find(params[:id])
     end
     
     def new
+        @post =Post.new
     end
     
-    def new
-    end
-    
+  
     def create
+        @post =Post.create(post_params)
+        if @post.valid?
+         redirect_to root_path
+         else
+         render :new
+        end
     end
     
     def edit
+         @posts = Post.find(params[:id])
     end
     
-    def updated
+    def update
+      @post = Post.find(params[:id])
+      @post.update(post_params)
+      if @post.valid?
+        redirect_to root_path
+      else
+        render :edit
+      end
     end
-    
+        
     def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+      redirect_to root_path
     end
-
+    
+    private
+    
+    def post_params
+        params.require(:post).permit(:title, :author, :content)
+    end
 end
